191e5bb263c8e743a33b7ffb315480448f145a46
Harshavardhan <187y1a0575@gmail.com>
2020-09-28 15:39:20 +0530

    Almost completed hello world website using svelte

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<div class='main'>
+	<h1>Hello Svelte world</h1>
+	<h2>Created by: Harshavardhan</h2>
+</div>
+
+<style>
+	.main {
+		text-align: center;
+	}
+
+	h1 {
+		font-weight: bolder;
+		font-size: 4rem;
+	}
+</style>
